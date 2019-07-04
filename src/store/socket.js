4540d8255cc27af0e5f07c4190d7485575939543
Vasilyev <a6.vasilyev@vasilyeva6-a.itransition.corp>
2019-07-04 13:43:05 +0300

    Fixes

--- src/store/socket.js
+++ src/store/socket.js
@@ -12,20 +12,6 @@ import { navigate } from './navigation/actions';
 let socket;
 let store;
 
-// const joinRoom = () => {
-//   if (this.pendingRoom !== currentRoom && this.originPublicKey) {
-//     this.addNotification(`Connecting to Room - ${this.pendingRoom}`);
-
-//     // Reset room state variables
-//     this.messages = [];
-//     this.destinationPublicKey = null;
-
-//     // Emit room join request.
-//     socket.emit('JOIN', this.pendingRoom);
-//   }
-// };
-
-
 export const configureSocket = (s) => {
   store = s;
 };
@@ -34,7 +20,6 @@ export const runSocket = (token) => {
   socket = io(API.DOMAIN, {
     query: { token },
   });
-  // socket.connect();
 
   socket.on('connect', () => {
 
@@ -47,22 +32,9 @@ export const runSocket = (token) => {
   });
 
   socket.on('DESTINATION_PUBLIC_KEY', ({ chatId, key }) => {
-    // console.log(`Public Key Received - ${key}, id --- ${chatId}`);
     store.dispatch(setDestinationPublicKey(chatId, key));
     getCurrentChat(chatId);
   });
-
-  // socket.on('NEW_CONNECTION', () => {
-  //   console.warn('Another user joined the room.');
-
-  //   // sendPublicKey();
-  // });
-
-  // socket.on('ROOM_JOINED', (newRoom) => {
-  //   // currentRoom = newRoom;
-  //   console.warn(`Joined Room - ${newRoom}`);
-  //   // sendPublicKey();
-  // });
 
   socket.on('SET_CURRENT_CHAT', (chat) => {
     const { _id } = store.getState().user;
@@ -84,7 +56,7 @@ export const runSocket = (token) => {
 
       Promise.all(decryptMessagesPromises).then((dec) => {
         if (chatIdx > -1) {
-          updatedChats[chatIdx] = { ...chat, messages: dec };
+          updatedChats[chatIdx] = { ...chat, messages: dec.reverse() };
         } else {
           updatedChats.push(chat);
         }
@@ -124,21 +96,10 @@ export const sendMessage = (message) => {
   socket.emit('send_message', message);
 };
 
-// export const joinConversation = (chatId, keys) => {
-//   const { _id } = store.getState().user;
-//   store.dispatch(setPublicKey(chatId, keys.publicKey));
-//   store.dispatch(setPrivateKey(chatId, keys.privateKey));
-//   // socket.emit('JOIN', { chatId });
-//   console.log('join');
-//   socket.emit('GET_CHAT', { chatId, userId: _id });
-// };
-
 export const getDestinationKeys = (chatId, userPublicKey) => {
-  const { _id: userId } = store.getState().user;
-  socket.emit('GET_DESTINATION_PUBLIC_KEY', { chatId, userPublicKey, userId });
+  socket.emit('GET_DESTINATION_PUBLIC_KEY', { chatId, userPublicKey });
 };
 
 export const getCurrentChat = (chatId) => {
-  const { _id: userId } = store.getState().user;
-  socket.emit('GET_CURRENT_CHAT', { chatId, userId });
+  socket.emit('GET_CURRENT_CHAT', { chatId });
 };
